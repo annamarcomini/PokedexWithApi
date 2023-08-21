b707e70ce49883dda38e63369242f30741bc6e9f
annamarcomini <annajuliamarcomini23@gmail.com>
2023-08-20 22:21:07 -0300

    fazendo um get na api do git

--- src/components/Repository.tsx
+++ src/components/Repository.tsx
@@ -1,12 +1,25 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import { RepositoryItem } from "./RepositoryItem"
+import axios from "axios"
+import { api } from "@/services/api"
 
 export function Repository(){
  const[repositories, setRepositories]= useState([])
+       useEffect(() => {
+         api
+           .get("https://api.github.com/users/joaovitorqueiroz/repos")
+           .then((response) => setRepositories(response.data)) // Atualiza o estado com os dados da resposta
+      },[])
+
+  
+
  return(
   <section className="repository"> 
   <h1>Lista de repositórios</h1>
   <ul>
-
+   {repositories.map(repository =>{
+    return <RepositoryItem repository={repository}/>
+   })}
   </ul>
   </section>
  )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,14 @@
 import styles from './home.module.scss'
 import Link from "next/link"
+import { Repository } from '@/components/Repository'
 
 export default function Home (){
 return (
   <>
     <div className={styles.container}>
       <section className={styles.hero}>
-        <span>Home</span>
+        
+        <Repository/>
        <Link href="/details">
           <button type="button">Details</button>
         </Link>
