df650e6e5ee7e102f2d743810cd47d318c97293b
annamarcomini <annajuliamarcomini23@gmail.com>
2023-09-02 20:23:36 -0300

    termino do search

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -1,5 +1,13 @@
+import styles from './styles.module.scss'
+
 export function Search(){
  return(
-  
+  <div className={styles.Container}>
+  <input type='text'
+  placeholder="Ex: Pikachu..."
+  >
+  </input>
+  <button type='button'>Buscar</button>
+  </div>
  )
 }
