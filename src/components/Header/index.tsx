cc93c15ce2122a8fcbc01897c13fbaabc3c7f153
annamarcomini <annajuliamarcomini23@gmail.com>
2023-09-02 20:24:19 -0300

    implantação do search no header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,17 @@
 import styles from "./styles.module.scss"
 import logo from "../../assets/logo.svg"
 import pokeball from "../../assets/pokeball.svg"
+import { Search } from "../Search"
 
 export function Header(){
 
  return (
    <>
-     <div className="Container">
+     <div className={styles.container}>
        <img src={logo} alt="pokemon" />
      </div>
-     <div className="Content">
+     <div className={styles.content}>
+      <Search/>
        <img src={pokeball} alt="pokeball" />
      </div>
    </>
