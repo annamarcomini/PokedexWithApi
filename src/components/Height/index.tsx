63df46bb05c5712f9fb9d40136fc8b1c02597a43
annamarcomini <annajuliamarcomini23@gmail.com>
2023-10-02 22:32:58 -0300

    ajustes de boas práticas de código

--- src/components/Height/index.tsx
+++ src/components/Height/index.tsx
@@ -8,7 +8,6 @@ interface HeightProps {
 export default function Height ({value}: HeightProps){
 return (
   <div className={styles.texts}>
-    <p>Height:</p>
     <span>{(value / 10).toFixed(2)}m</span>
     {/* para concatenar de acordo com as casas decimais */}
   </div>
